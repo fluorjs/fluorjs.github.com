ac6cd2209aba22061c403602fb7a638bdce54f06
madx <madx+github@yapok.org>
2020-05-13 08:23:12 +0200

    feat: Better index page and handbook page stub

--- site/index.js
+++ site/index.js
@@ -1,7 +1,27 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-import { Fluor, FluorSource, Page } from "components"
+import { Button, Fluor, FluorSource, Page } from "components"
+
+function Menu() {
+  return (
+    <ul className="flex justify-center items-center">
+      {[
+        { text: "Handbook", href: "/handbook/" },
+        { text: "GitHub", href: "https://github.com/fluorjs/fluor" },
+      ].map(({ text, href }) => (
+        <li key={href}>
+          <a
+            className="py-4 px-6 text-yellow-700 hover:text-yellow-500 font-bold text-lg leading-none"
+            href={href}
+          >
+            {text}
+          </a>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 export default function Index() {
   return (
@@ -15,26 +35,24 @@ export default function Index() {
           <h1 className="font-maven text-4xl sm:text-5xl text-green-400 tracking-wide leading-none mt-6">
             Fluor.js
           </h1>
-          <p className="text-xl sm:text-2xl text-green-900 text-center mt-4 mb-8">
+          <p className="text-xl sm:text-2xl text-green-900 text-center mt-4">
             Sprinkle interactivity on your design
           </p>
         </header>
 
-        <article className="max-w-full">
-          <FluorSource>
-            {`
-              on("click", "button", toggle("wobble", "header"))
-              on("animationend", "header", toggle("wobble"))
-            `}
-          </FluorSource>
-          <div className="mt-2 flex flex-wrap justify-center">
-            <button className="py-4 px-6 text-lg mr-4 mt-4">Try it!</button>
-            <a
-              className="rounded py-4 px-6 mt-4 bg-white text-green-900 hover:text-green-700 font-bold text-lg shadow-lg leading-none"
-              href="https://github.com/fluorjs/fluor/"
-            >
-              Get Started
-            </a>
+        <article className="max-w-full mt-6">
+          <Menu />
+
+          <div className="flex flex-col sm:flex-row mt-6">
+            <FluorSource className="rounded-b-none sm:rounded-l sm:rounded-r-none">
+              {`
+                on("click", "button", toggle("wobble", "header"))
+                on("animationend", "header", toggle("wobble"))
+              `}
+            </FluorSource>
+            <Button className="rounded-t-none sm:rounded-r sm:rounded-l-none">
+              Run
+            </Button>
           </div>
         </article>
 
