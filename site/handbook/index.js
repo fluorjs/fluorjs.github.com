4f5f6c7024e88178a46721b91901ca746ca06d5c
madx <madx+github@yapok.org>
2020-05-14 08:30:16 +0200

    feat: Update home page

--- site/handbook/index.js
+++
@@ -1,3 +0,0 @@
-export default function Handbook() {
-  return "Under construction"
-}
--- site/index.js
+++ site/index.js
@@ -3,65 +3,83 @@ import { Helmet } from "react-helmet"
 
 import { Button, Fluor, FluorSource, Page } from "components"
 
-function Menu() {
-  return (
-    <ul className="flex justify-center items-center">
-      {[
-        { text: "Handbook", href: "/handbook/" },
-        { text: "GitHub", href: "https://github.com/fluorjs/fluor" },
-      ].map(({ text, href }) => (
-        <li key={href}>
-          <a
-            className="py-4 px-6 text-yellow-700 hover:text-yellow-500 font-bold text-lg leading-none"
-            href={href}
-          >
-            {text}
-          </a>
-        </li>
-      ))}
-    </ul>
-  )
-}
-
 export default function Index() {
   return (
     <Page>
       <Helmet>
         <link rel="stylesheet" href="/assets/home.css" />
       </Helmet>
-      <main className="max-w-3xl h-screen mx-auto p-8 flex flex-col items-center justify-center">
-        <header className="flex flex-col items-center justify-center">
-          <img src="/assets/logo.svg" alt="" className="w-24" />
-          <h1 className="font-maven text-4xl sm:text-5xl text-green-400 tracking-wide leading-none mt-6">
-            Fluor.js
-          </h1>
-          <p className="text-xl sm:text-2xl text-green-900 text-center mt-4">
-            Sprinkle interactivity on your design
-          </p>
-        </header>
 
-        <article className="max-w-full mt-6">
-          <Menu />
+      <nav className="p-4 flex justify-center items-center bg-green-400">
+        <input
+          type="search"
+          placeholder="Search docs..."
+          className="w-48 bg-green-800 text-green-100 rounded-lg p-2 placeholder-green-200 border-2 border-green-500 shadow-inner"
+        />
+      </nav>
 
-          <div className="flex flex-col sm:flex-row mt-6">
-            <FluorSource className="rounded-b-none sm:rounded-l sm:rounded-r-none">
-              {`
-                on("click", "button", toggle("wobble", "header"))
-                on("animationend", "header", toggle("wobble"))
-              `}
-            </FluorSource>
-            <Button className="rounded-t-none sm:rounded-r sm:rounded-l-none">
-              Run
-            </Button>
-          </div>
-        </article>
+      <header className="max-w-3xl mx-auto px-4 py-12 sm:py-20 flex flex-col items-center justify-center">
+        <h1 className="font-maven text-5xl sm:text-6xl text-green-400 tracking-wide leading-none mt-6 flex items-center justify-center flex-wrap">
+          <img
+            src="/assets/logo.svg"
+            alt=""
+            className="w-12 h-12 sm:w-16 sm:h-16 mr-4 mb-2 sm:mb-0"
+          />
+          Fluor.js
+        </h1>
+        <h2 className="text-lg sm:text-xl text-gray-600 text-center mt-4">
+          Sprinkle interactivity on your design
+        </h2>
+
+        <div className="max-w-full flex flex-col mt-10 sm:mt-20">
+          <FluorSource className="rounded mb-4">
+            {`
+              on("click", "button", toggle("wobble", "h1"))
+              on("animationend", "h1", toggle("wobble"))
+            `}
+          </FluorSource>
+          <Button className="rounded-full px-4 py-4 self-center flex">
+            <svg
+              className="fill-current w-4 h-4 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M4 0 L4 24 L24 12 Z" />
+            </svg>
+            Try now
+          </Button>
+        </div>
 
         <Fluor>
           {`
-            on("click", "button", toggle("wobble", "header"))
-            on("animationend", "header", toggle("wobble"))
+            on("click", "button", toggle("wobble", "h1"))
+            on("animationend", "h1", toggle("wobble"))
           `}
         </Fluor>
+      </header>
+
+      <main className="max-w-3xl mx-auto px-8">
+        <article id="intro">
+          <h2>What is it?</h2>
+          <p>
+            Fluor.js is a small JavaScript library that provides a high-level
+            language for easily adding interactions and effects to your
+            websites.
+          </p>
+          <p>
+            It is great for prototypes, UI and UX research and for all websites
+            that do not require the cumbersome machinery of a full-fledged
+            framework.
+          </p>
+          <p>
+            It is inspired by <a href="http://uilang.com">uilang</a> and{" "}
+            <a href="https://github.com/alpinejs/alpine">Alpine.js</a>, has 0
+            dependencies and can be added to any page with a single line of
+            HTML.
+          </p>
+        </article>
       </main>
     </Page>
   )
