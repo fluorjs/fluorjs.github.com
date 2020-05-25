9ba86e2cb4cf9d0587c2f736497351fba419792b
madx <madx+github@yapok.org>
2020-05-25 15:13:43 +0200

    feat(site): Add GitHub link

--- site/index.js
+++ site/index.js
@@ -134,18 +134,33 @@ export default function Index() {
               on("animationend", "h1", removeClass("wobble"))
             `}
           </Source>
-          <Button className="self-center flex">
-            <svg
-              className="fill-current w-4 h-4 mr-2"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
+          <div className="flex items-center justify-center">
+            <Button className="self-center flex">
+              <svg
+                className="fill-current w-4 h-4 mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M4 0 L4 24 L24 12 Z" />
+              </svg>
+              Try now
+            </Button>
+            <a
+              href="https://github.com/fluorjs/fluor"
+              className="rounded-full p-4 shadow-md leading-none focus:outline-none focus:shadow-outline bg-gray-400 text-gray-900 hover:text-gray-800 hover:bg-gray-300 ml-4"
             >
-              <path d="M4 0 L4 24 L24 12 Z" />
-            </svg>
-            Try now
-          </Button>
+              <svg
+                class="fill-current w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <title>GitHub</title>
+                <path d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0"></path>
+              </svg>
+            </a>
+          </div>
         </div>
 
         <Fluor>
